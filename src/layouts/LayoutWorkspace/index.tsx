86c327273f13ba31298dce151eb5fdec1f0e6d39
van anh <info.anhdev@gmail.com>
2023-04-24 16:45:14 +0700

    Desgined sidebar ui

--- src/layouts/LayoutWorkspace/index.tsx
+++ src/layouts/LayoutWorkspace/index.tsx
@@ -1,26 +1,15 @@
-import { Col, Row } from 'antd';
+
 import React from 'react';
+import Sidebar from '~/components/Sidebar/sidebar';
 import { IChildrenComponentProps } from '~/shared/model/global';
 const LayoutWorkspace: React.FC<IChildrenComponentProps> = ({ children }) => {
    return (
       <div>
          <nav></nav>
-         <Row>
-            <Col
-               md={{ span: 6, offset: 0 }}
-               xs={{ span: 6, offset: 0 }}
-               lg={{ span: 6, offset: 0 }}
-            >
-               sidebar
-            </Col>
-            <Col
-               md={{ span: 18, offset: 0 }}
-               xs={{ span: 18, offset: 0 }}
-               lg={{ span: 18, offset: 0 }}
-            >
-               <div>{children}</div>
-            </Col>
-         </Row>
+         <div className="wrapper" style={{display: "flex"}}>
+            <Sidebar />
+            <div className="content" style={{flex : 1, backgroundColor: "red"}}>{children}</div>
+         </div>
       </div>
    );
 };
