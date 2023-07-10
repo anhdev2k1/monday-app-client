a37d21c91d6275bb1a84bc8dc04a1c6ce6acba18
anh dev <info.anhdev@gmail.com>
2023-07-10 21:29:02 +0700

    🚀

--- src/config/axios-interceptor.ts
+++ src/config/axios-interceptor.ts
@@ -3,7 +3,7 @@ import { SERVER_API_URL } from './constants';
 
 const TIMEOUT = 1 * 60 * 1000;
 axios.defaults.timeout = TIMEOUT;
-axios.defaults.baseURL = SERVER_API_URL;
+axios.defaults.baseURL = process.env.REACT_APP_SERVER_API_URL;
 axios.defaults.withCredentials = true;
 
 const setupAxiosInterceptors = () => {
